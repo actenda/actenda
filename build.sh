a07cc8d2c645d4146142cb2b0c0f5c002ecd3451
vinz243 <vinz243@gmail.com>
2018-06-26 09:22:47 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,14 +6,13 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout -b gh-pages
   git add . *.html dist/*
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/actenda/actenda.github.io.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
+  git remote add origin https://${GH_TOKEN}@github.com/actenda/actenda
+  git subtree push --prefix dist origin gh-pages
 }
 
 setup_git
