654b99011a2604dce48c9969cdbc8722c6416640
Travis CI <travis@travis-ci.org>
2018-06-30 08:32:25 -0400

    Add ponyfill

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,6 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import './style.scss';
+import 'babel-polyfill';
 
 render(<App />, document.getElementById('root'));
